3f053c1645fbade3f5157f297b9e2ff6fba7d1bd
francois jullion <18002886@e103m02.istic.univ-rennes1.fr>
2020-02-12 08:39:39 +0100

    Add all required files

---
+++ Part1/sysprog-audio-1.5/lecteur.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <strings.h>
+#include "audio.h"
+
+int main() {
+
+        printf("Quel fichier audio jouer ? ");    
+
+
+
+
+}
