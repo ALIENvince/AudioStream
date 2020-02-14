becc418ff6012cc7546a217843e6070c3b1c2d8b
Francis-Istic <francis@LAPTOP-KE7BGEOK.localdomain>
2020-02-14 16:42:15 +0100

    finish first script

--- Part1/sysprog-audio-1.5/src/lecteur.c
+++ Part1/sysprog-audio-1.5/src/lecteur.c
@@ -6,19 +6,46 @@
 
 int main(int argc, char * argv []) {
 	
-	char * track = argv[0]; 
+	char * track = argv[1]; 
 
-	int * s_rate = NULL;	
-	int * s_size = NULL;
-	int * channel = NULL;
+	int s_rate;	
+	int s_size;
+	int channel;
 	
-	int fd = aud_readinit(track,s_rate,s_size,channel);
+	/* Création du descripteur de fichier de lecture*/
+	int read_fd = aud_readinit(track,&s_rate,&s_size,&channel);
 
-	if(fd == -1 ) {
+	if(read_fd == -1 ) {
 		fprintf(stderr,"Can't open the file");
 		exit(1);
 	}
+	
+	/* Modification des valeurs pour les filtres */
 
+	/*-------------------------------------------*/
+	
+
+	/* Création du descripteur de fichier d'ouverture*/
+	int write_fd = aud_writeinit(s_rate,s_size,channel);
+
+	if(write_fd == -1) {
+		fprintf(stderr,"Can't write");
+		exit(1);
+	}
+	
+	/* Boucle de lecture et ecriture*/
+	int read_value;
+	int write_value;
+
+	char buffer[s_size];
+
+	while( (read_value == s_size) && (write_value == s_size) ) {
+		read_value = read(read_fd, buffer,s_size);
+		write_value = write(read_fd,buffer,s_size);		
+	}
+
+	close(read_fd);
+	close(write_fd);
 	return 0;
 
 
